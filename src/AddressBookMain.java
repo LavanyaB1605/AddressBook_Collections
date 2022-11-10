1e5bbb05bab51b9de26b53f3906e70ce9c3df48b
LavanyaB1605 <lavanya.bhaskaran16@gmail.com>
2022-11-10 18:27:58 +0530

    Added AddContact Details

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,8 +1,52 @@
-public class AddressBookMain {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-        public static void main(String args[]){
-            System.out.println("Welcome to Address Book");
-            Person person = new Person();
-        }
+public class AddressBookMain {
+    private static Scanner sc = new Scanner(System.in);
+    ArrayList addressBook;
+    AddressBookMain() {
+        addressBook = new ArrayList<>();
     }
+        public static void main(String args[]) {
+            System.out.println("Welcome to Address Book");
+            boolean isExit = false;
+            AddressBookMain addressBook = new AddressBookMain();
+            while (!isExit) {
+                System.out.println("Enter Option" +
+                        "\n1. Add Person" +
+                        "\n2. Edit Person" +
+                        "\n3. Delete Person" +
+                        "\n4. Show Address Book" +
+                        "\n5. Exit");
+                int option = Integer.parseInt(sc.nextLine());
+                switch (option) {
+                    case 1:
+                        addressBook.addPerson();
+                        break;
+                }
+
+            }
+        }
+
+    private void addPerson() {
+        System.out.println("Enter First name");
+        String firstName = sc.nextLine();
+        System.out.println("Enter Last Name");
+        String lastName = sc.nextLine();
+        System.out.println("Enter Street");
+        String street = sc.nextLine();
+        System.out.println("Enter City");
+        String city = sc.nextLine();
+        System.out.println("Enter State");
+        String state = sc.nextLine();
+        System.out.println("Enter Phone Number");
+        long mobileNumber = sc.nextLong();
+        //parameterized constructor calling of Contact person class
+        Person person = new Person(firstName, lastName, street, city, state, mobileNumber);
+        //Adding object element in arrayList
+        addressBook.add(person);
+
+    }
+}
 
--- src/Person.java
+++ src/Person.java
@@ -9,12 +9,23 @@ public class Person {
     public Person() {
     }
 
-    public Person(String firstName,String lastName,String street,String city,String state,long mobileNumber) {
+    public Person(String firstName, String lastName, String street, String city, String state, long mobileNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.street = street;
         this.city = city;
         this.state = state;
         this.mobileNumber = mobileNumber;
+    }
+    @Override
+    public String toString() {
+        return "Person{" +
+                "FirstName='" + firstName + '\'' +
+                ",LastName='" + lastName + '\'' +
+                ",Street='" + street + '\'' +
+                ",City='" + city + '\'' +
+                ",State='" + state + '\'' +
+                ",MobileNumber'" + mobileNumber + '\'' +
+                '}';
     }
 }
